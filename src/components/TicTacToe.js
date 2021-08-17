dd5ff9c244c9f53cf4d1e76fff439c27ca67ac04
Cabidge <cabbageboi69@gmail.com>
2021-08-16 21:27:13 -0400

    Added cell status update on click

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -3,9 +3,16 @@ import CellContainer from "./CellContainer";
 
 function TicTacToe() {
     const [cells, setCells] = useState(Array(9).fill(null));
+    const [playerId, setPlayerId] = useState(0);
 
     function handleClick(index) {
-        console.log(index);
+        // Set cell at the given index to playerId
+        const newCells = [...cells];
+        newCells[index] = playerId;
+        setCells(newCells);
+
+        // Swaps playerId from 0 to 1 or vice versa
+        setPlayerId((playerId + 1) % 2);
     }
 
     return <CellContainer cellStatuses={cells} onCellClick={handleClick} />;
